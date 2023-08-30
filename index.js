9615c212bd2261a38b50d1a87365b71a61ae22fc
nglethienminh <nglethien.minh@gmail.com>
2023-08-30 09:22:40 +0700

    update

--- index.js
+++ index.js
@@ -43,7 +43,14 @@ const fileFilter = (req, file, cb) => {
 // app.use(allowCors());
 // setup for call api
 // NOTE: need to change origin url when deploy to firebase
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://ecommerce-asm3.web.app/",
+      "https://ecommerce-asm3-admin.web.app/",
+    ],
+  })
+);
 app.use(express.json());
 
 app.use(
@@ -113,25 +120,25 @@ app.use((req, res, next) => {
 //   });
 // });
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   // another common pattern
+//   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+//   );
+//   if (req.method === "OPTIONS") {
+//     res.status(200).end();
+//     return;
+//   }
+//   next();
+// });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
