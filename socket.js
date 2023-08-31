73da461637e3835cc95ae6c8675e51c6692d9ba7
nglethienminh <nglethien.minh@gmail.com>
2023-08-31 08:40:32 +0700

    cors socket

--- socket.js
+++ socket.js
@@ -3,9 +3,10 @@ module.exports = {
   init: (httpServer) => {
     io = require("socket.io")(httpServer, {
       cors: {
-        origin: ["http://localhost:3000", "http://localhost:3001"],
-        methods: ["PUT", "GET", "POST", "DELETE", "OPTIONS"],
-        credentials: false,
+        // origin: ["http://localhost:3000", "http://localhost:3001"],
+        // methods: ["PUT", "GET", "POST", "DELETE", "OPTIONS"],
+        // credentials: false,
+        origin: "*",
       },
     });
     return io;
