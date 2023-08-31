7b41381b3ea39a3825538bddcebbb0cee2667bd4
nglethienminh <nglethien.minh@gmail.com>
2023-08-31 10:18:15 +0700

    fix socketio

--- socket.js
+++ socket.js
@@ -6,7 +6,12 @@ module.exports = {
         // origin: ["http://localhost:3000", "http://localhost:3001"],
         // methods: ["PUT", "GET", "POST", "DELETE", "OPTIONS"],
         // credentials: true,
-        origin: "*",
+        origin: [
+          "https://ecommerce-asm3.web.app",
+          "https://ecommerce-asm3-admin.web.app",
+          "http://localhost:3000",
+          "http://localhost:3001",
+        ],
         credentials: true,
       },
     });
